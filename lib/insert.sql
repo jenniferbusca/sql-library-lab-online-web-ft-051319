701bda446c7c324c92f4749934d2f0c1fdf0c5d1
jenniferbusca <jennifer.busca@gmail.com>
2019-06-11 22:01:22 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,46 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES
+(NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL);
+
+INSERT INTO subgenres (id, name) VALUES
+(NULL,NULL),
+(NULL,NULL);
+
+INSERT INTO authors (id, name) VALUES
+(NULL,NULL),
+(NULL,NULL);
+
+INSERT INTO books (id, title, year, series_id) VALUES
+(NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL);
+
+INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES
+(NULL,NULL,NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL,NULL,NULL),
+(NULL,NULL,NULL,NULL,NULL,NULL);
+
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
+INSERT INTO character_books (character_id, book_id) VALUES (NULL, NULL);
